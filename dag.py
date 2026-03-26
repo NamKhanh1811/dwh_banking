91f724026ca350607d770770e6f2875c90fb5a77
NamKhanh1811 <khanhnn1811@gmail.com>
2026-03-26 14:02:26 +0700

    dag

---
+++ dag.py
@@ -0,0 +1,18 @@
+from airflow import DAG
+from airflow.operators.bash import BashOperator
+from datetime import datetime
+
+with DAG(
+    dag_id='my_dbt_transformation',
+    start_date=datetime(2024, 1, 1),
+    schedule_interval=None,
+    catchup=False
+) as dag:
+
+    run_dbt = BashOperator(
+        task_id='dbt_run_task',
+        bash_command="""
+            cd /opt/airflow/dags/repo && \
+            dbt run --profiles-dir . --project-dir .
+        """
+    )
